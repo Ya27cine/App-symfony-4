23c14e7bc17b8543849c598a573697c051398f98
KHELIFA Yassine <prostam27@gmail.com>
2021-08-31 20:37:36 +0200

    Image entité et @Uploadable annotation

---
+++ src/Entity/Image.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=ImageRepository::class)
+ * @Vich\Uploadable()
+ */
+class Image
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @Vich\UploadableField(mapping="images", fileNameProperty="url")
+     */
+    private $file;
+
+    /**
+     * @ORM\Column(type="string", length=255,  nullable=true)
+     */
+    private $url;
+
+    /**
+     * @ORM\Column(type="string", length=120, nullable=true)
+     */
+    private $alt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getAlt(): ?string
+    {
+        return $this->alt;
+    }
+
+    public function setAlt(?string $alt): self
+    {
+        $this->alt = $alt;
+
+        return $this;
+    }
+}
