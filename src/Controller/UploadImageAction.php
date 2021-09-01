5fc120f46d23761727ed56d08b3555458148af8b
KHELIFA Yassine <prostam27@gmail.com>
2021-09-01 23:22:41 +0200

    Implémenter une méthode personnaliser pour Upload partie 1

--- src/Controller/UploadImageAction.php
+++ src/Controller/UploadImageAction.php
@@ -1,11 +1,37 @@
 <?php 
 namespace App\Controller;
 
+use Symfony\Component\Form\FormFactoryInterface;
+use App\Entity\Image;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
 class UploadImageAction{
 
-    public function __construct()
+    private $formFactoryInterface;
+    private $em;
+    public function __construct(FormFactoryInterface $formFactoryInterface, EntityManagerInterface $em)
     {
-        
+        $this->formFactoryInterface = $formFactoryInterface;
+        $this->em = $em;
+    }
+
+    public function __invoke(Request $request)
+    {
+        $image = new Image();
+
+        // validate the form
+        $form = $this->formFactoryInterface->create(null, $image);
+        $form->handleRequest( $request );
+
+        if($form->isSubmitted() && $form->isValid() ){
+            $this->em->persist( $image );
+            $this->em->flush();
+
+            return $image;
+
+        }
+
     }
 
 }
